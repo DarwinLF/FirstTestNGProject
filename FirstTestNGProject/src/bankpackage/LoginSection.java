27d25e0f9421200148a7736910e71769d79a1622
DarwinLF <darwinlf@outlook.com>
2024-03-28 20:45:39 -0400

    Fixing day 2 code

--- FirstTestNGProject/src/bankpackage/LoginSection.java
+++ FirstTestNGProject/src/bankpackage/LoginSection.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.*;
+
 import java.io.IOException;
 import java.time.Duration;
 
@@ -18,13 +19,14 @@ public class LoginSection {
     
     @BeforeClass
     public void setUp() throws IOException {
-    	setupFirefox();
+    	FirefoxOptions options = new FirefoxOptions();
+        options.setProfile(new FirefoxProfile());
+        driver = new FirefoxDriver(options);
         
         // Load base URL and wait time from properties
-        baseUrl = Util.getBaseUrl();
-        waitTime = Util.getWaitTime();
-        
-        driver.get(baseUrl);
+        baseUrl = Util.BASE_URL;
+        waitTime = Util.WAIT_TIME;
+        driver.get(baseUrl + "/V4/");
     }
     
     @Test
@@ -35,25 +37,16 @@ public class LoginSection {
         WebElement passwordField = driver.findElement(By.name("password"));
         WebElement loginButton = driver.findElement(By.name("btnLogin"));
     	
-    	userField.sendKeys(Util.getUser());
-    	passwordField.sendKeys(Util.getPassword());
-    	
+    	userField.sendKeys(Util.USER_NAME);
+    	passwordField.sendKeys(Util.PASSWD);
     	loginButton.click();
     	
-    	WebElement actualWelcomeMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("marquee.heading3")));
-        String expectedWelcomeMessage = "Welcome To Manager's Page of Guru99 Bank";
-        Assert.assertEquals(actualWelcomeMessage.getText(), expectedWelcomeMessage);
+    	String actualTitle = driver.getTitle();
+        Assert.assertEquals(actualTitle, Util.EXPECT_TITLE);
     }
     
     @AfterClass
     public void tearDown() {
     	driver.quit();
-    }
-    
-    private void setupFirefox() {
-        // Setup Firefox with specified location and profile
-        FirefoxOptions options = new FirefoxOptions();
-        options.setProfile(new FirefoxProfile());
-        driver = new FirefoxDriver(options);
     }
 }
--- FirstTestNGProject/src/bankpackage/Util.java
+++ FirstTestNGProject/src/bankpackage/Util.java
@@ -1,36 +1,18 @@
 package bankpackage;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Properties;
-
 public class Util {
-    private static Properties prop = new Properties();
+	// Setting Base URL
+	public static final String BASE_URL = "http://www.demo.guru99.com/";
     
-    static {
-        try {
-            FileInputStream fis = new FileInputStream("resources/config.properties");
-            prop.load(fis);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    
-    public static String getBaseUrl() {
-        return prop.getProperty("BaseURL");
-    }
-    
-    public static int getWaitTime() {
-    	return Integer.parseInt(prop.getProperty("WaitTime"));
-    }
-    
-    public static String getUser() {
-    	return prop.getProperty("User");
-    }
-    
-    public static String getPassword() {
-    	return prop.getProperty("Password");
-    }
-    
-    // Add methods to retrieve other parameters as needed (e.g., username, password)
+	// Time to wait when searching for a GUI element 
+	public static final int WAIT_TIME = 30; 
+											
+
+	// Valid account for login
+	public static final String USER_NAME = "mngr563045";
+	public static final String PASSWD = "ejupUzu";
+
+
+	// Expected output
+	public static final String EXPECT_TITLE = "Guru99 Bank Manager HomePage";
 }
